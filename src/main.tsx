79f7a2376bb2129d0b7e5cae5d18bf5fc71937de
Hamilton Lumati <hamiltonlumati@gmail.com>
2024-01-17 00:55:38 +0100

    Redux Addition

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'
 import './index.css';
 import Screen from './layout/screen';
+import store from './app/store';
+import { Provider } from 'react-redux';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Screen />  
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <Screen />  
+    </React.StrictMode>
+  </Provider>
 );
