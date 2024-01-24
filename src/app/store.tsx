487e24fd9ed8d818b1cde524f0fc3395035ad5d7
hamiltonlumati <hamiltonlumati@gmail.com>
2024-01-24 22:39:11 +0100

    generator Reducer update

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import generatorReducer from '../assets/quoteGenerator/quoteGeneratorSlice';
 
 export const store = configureStore({
-    reducer:{},
+    reducer:{
+        generator: generatorReducer,
+    },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,4 +1,10 @@
+import type { RootState } from '../app/store';
+import { useSelector, useDispatch } from 'react-redux';
+import { generate } from '../assets/quoteGenerator/quoteGeneratorSlice';
+
 function Card(){
+    const dispatch = useDispatch();
+    
     return(
         <div className="w-1/2 mx-auto bg-white p-10 rounded text-blue">
             <p className=" ">É a vida!</p>
