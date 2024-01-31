ccbba778a90dc14de67fea26393b7e3aefc572fe
hamiltonlumati <hamiltonlumati@gmail.com>
2024-02-01 00:15:52 +0100

    update

--- src/assets/quoteGenerator/quoteGeneratorSlice.tsx
+++ src/assets/quoteGenerator/quoteGeneratorSlice.tsx
@@ -4,48 +4,48 @@ const quotes: { quote: string, author: string, color: string }[] = [
     { 
         //0
         "quote": 'Technique without art is shallow and doomed. Art without technique is insulting.',
-        "author": "Dorthy Alexander",
+        "author": "- Dorthy Alexander",
         "color": "blue" 
     },
     { 
         //1
         "quote": 'Anything that was in the world when you were born is normal and natural. Anything invented between when you were 15 and 35 is new and revolutionary and exciting, and you will probably get a career in it. Anything invented after you are 35 is against the natural order of things. ',
-        "author": "Douglas Adams",
+        "author": "- Douglas Adams",
         "color": "purple" },
     { 
         //2
         "quote": 'Science and technology multiply around us. To an increasing extent they dictate the languages in which we speak and think. Either we use those languages, or we remain mute. ',
-        "author": "J. G. Ballard",
+        "author": "- J. G. Ballard",
         "color": "amber" 
     },
     {
         //3
         "quote": 'Engineering or Technology is the making of things that did not previously exist, whereas science is the discovering of things that have long existed.',
-        "author": 'David Billington',
+        "author": '- David Billington',
         "color": 'orange',
     },
     {
         //4
         "quote": 'The goal of the future is full unemployment, so we can play. That’s why we have to destroy the present politico-economic system.',
-        "author": 'Arthur C. Clarke',
+        "author": '- Arthur C. Clarke',
         "color": 'cyan'
     },
     {
         //5
         "quote": 'Technological progress has merely provided us with more efficient means for going backwards. ',
-        "author": 'Aldous Huxley',
+        "author": '- Aldous Huxley',
         "color": 'yellow'
     },
     {
         //6
         "quote": 'Curiosity and the urge to solve problems are the emotional hallmarks of our species; and the most characteristically human activities are mathematics, science, technology, music and the arts.',
-        "author": 'Carl Sagan',
+        "author": '- Carl Sagan',
         "color": 'gray'
     },
     {
         //7
         "quote": 'We live in a society exquisitely dependent on science and technology, in which hardly anyone knows anything about science and technology. ',
-        "author": 'Carl Sagan',
+        "author": '- Carl Sagan',
         "color": 'amber'
     }
 ];
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -13,7 +13,16 @@ function Card(){
         <div className={"w-1/2 mx-auto bg-white p-10 rounded text-"+color}>
             <p className="text-2xl">{quote}</p>
             <p className="text-right text-lg">{author}</p>
-            <button type="button" className="p-1 rounded bg-green hover:bg-green-dark text-white text-lg" onClick={() => dispatch(generate())}>Generate</button>
+            <div className='flex flex-row mt-5'>
+                <button type='button' className={'basis-1/4 rounded mx-2 text-white p-1 bg-'+color}>
+                    <a href={"https://twitter.com/intent/tweet?text="+quote+" -"+author}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi m-auto hover:text-gray-light bi-twitter-x" viewBox="0 0 16 16">
+                            <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
+                        </svg>
+                    </a>
+                </button>
+                <button type="button" className="basis-2/4  p-1 rounded bg-green hover:bg-green-dark text-white text-lg justify-self-end" onClick={() => dispatch(generate())}>Generate</button>
+            </div>
         </div>
     );
 };
